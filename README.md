42b211b692522f20248c62d175e5da6b24f487cd
Rob Bentley <robertbentley96@hotmail.co.uk>
2025-06-27 11:30:10 +0100

    Add README

---
+++ README.md
@@ -0,0 +1,17 @@
+# YouTube Viewer App
+
+## About This Project
+
+This is an Angular web app for making searches on YouTube via the YouTube API. There are set search terms which can be included in search requests and the top results are shown. These videos are then viewable in the app. This app was created as an alternative to the YouTube website as it serves many suggested videos to the user which is not always wanted.
+
+## Running This Project
+
+To run this project, you must have node installed.
+
+Open the youtube-viewer-app folder in your and run 'npm i' to install the node packages.
+
+Then run 'npm generate environments' to generate the environment files. Add the YOUTUBE_API_KEY setting to the environment.ts file with a valid API Key.
+
+Finally run 'npm run start' to serve the application.
+
+![YouTube Viewer App](images/YouTubeViewer.png)
--- youtube-viewer-app/src/app/search.service.ts
+++ youtube-viewer-app/src/app/search.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { YouTubeResponse } from './models/youtube-response';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,11 @@ import { YouTubeResponse } from './models/youtube-response';
 export class SearchService {
 
   private apiUrl = 'https://www.googleapis.com/youtube/v3';
-  private apiKey = 'X';
+  private apiKey = environment.YOUTUBE_API_KEY;
   
   constructor(private http: HttpClient) { }
 
   getSearchResults(searchTerm: string): Observable<YouTubeResponse> {
-    return this.http.get<YouTubeResponse>(this.apiUrl + '/search?type=video&part=snippet&maxResult=6&key=' + this.apiKey + '&q=' + searchTerm);
+    return this.http.get<YouTubeResponse>(this.apiUrl + '/search?type=video&part=snippet&maxResult=6&videoDuration=medium&key=' + this.apiKey + '&q=' + searchTerm);
   }
 }
